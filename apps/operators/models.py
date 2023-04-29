9ead95cdcd678b6a4636dc645631a21466c56244
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2023-04-29 12:51:21 -0500

    fix migrations

--- apps/operators/models.py
+++ apps/operators/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Permission (models.Model):
+  level = models.PositiveSmallIntegerField()
+  description = models.CharField(max_length=120)
+
+
+class Operator (models.Model):
+  user = models.ManyToManyField(User)
+  permission = models.OneToOneField(Permission, on_delete=models.SET_NULL,null=True)
+
+
--- tkchat/settings/base.py
+++ tkchat/settings/base.py
@@ -4,6 +4,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+# Initialise environment variables 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
