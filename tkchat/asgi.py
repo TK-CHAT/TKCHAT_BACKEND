89e2cb2629e4e9abbde0a96dd47fea8d0536e150
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2023-06-25 19:46:04 -0500

    fix asgi

--- tkchat/asgi.py
+++ tkchat/asgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+IS_DEV = 'RENDER' not in os.environ
+CURRENT_ENV = 'local'if IS_DEV else'production'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tkchat.settings.'+CURRENT_ENV)
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,10 +22,7 @@ from channels.http import AsgiHandler
 from .urls import urlpatterns
 import apps.livechat.routing
 
-IS_DEV = 'RENDER' not in os.environ
-CURRENT_ENV = 'local'if IS_DEV else'production'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tkchat.settings.'+CURRENT_ENV)
 application = ProtocolTypeRouter({
   'http':AsgiHandler(),
   'websocket': URLRouter(apps.livechat.routing.websockets_urlpatterns),
